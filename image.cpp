5c7061f54b897fe5cad3b10732916446dfe837fc
BenSnow6 <bsnow@bournemouth.ac.uk>
2019-10-25 14:03:12 +0100

    broket set and get functions

--- image.cpp
+++ image.cpp
@@ -15,6 +15,24 @@ unsigned char * image::pixels()
     return m_pixels.get();
 }
 
+
+void image::setPixel(unsigned int _x, unsigned int _y, unsigned char _r, unsigned char _g, unsigned char _b)
+{
+//calculate index into array fir red (then +1 green then +2 blue)
+    size_t index=0;
+    m_pixels[index]=_r;
+    m_pixels[index+1]=_g;
+    m_pixels[index+2]=_b;
+}
+
+void image::getPixel(unsigned int _x, unsigned int _y, unsigned char &o_r, unsigned char &o_g, unsigned char &o_b)
+{
+    size_t index=0;
+    m_pixels[index]=_r;
+    m_pixels[index+1]=_g;
+    m_pixels[index+2]=_b;
+}
+
 unsigned int image::width() const
 {
     return m_width;
--- imageTests.cpp
+++ imageTests.cpp
@@ -19,5 +19,16 @@ TEST(image,userCtor)
     EXPECT_EQ(i.height(),100);
     EXPECT_EQ(i.channels(),3);
     EXPECT_NE(i.pixels(), nullptr); //check the default value of i.pixels is the null pointer
+}
+
+TEST(image,setPixel)
+{
+    image i(100,100);
+    i.setPixel(0,0,255,0,0); //position followed by colour (x,y,r,g,b)
+    unsigned char r,g,b;
+    i.getPixel(0,0,r,g,b);
+    EXPECT_TRUE(r==255);
+    EXPECT_TRUE(g==0);
+    EXPECT_TRUE(b==0);
 
 }
